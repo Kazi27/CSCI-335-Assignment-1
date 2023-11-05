22dbcf304bc96ed216c6147d40c3ac676f50c1f1
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-04 23:45:51 -0400

    chngd regex comentd out if stat in copy constructr

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -28,10 +28,12 @@ bool ActionCard::isPlayable()
     } 
 
     //std::regex pattern("((?:DRAW|PLAY)\s([1-9]\d{0,1})\sCARD\(S\))"); //instruction starts w draw or play, has a space, 2 spaces for digits that can be 1 - 9, ends w string "CARDS" - 0 not included, case sensitive
-    std::regex pattern("((R?:DRAW|PLAY)\s([1-9]\d{0,1})\sCARD\(S\))"); //gradescope version
+    //std::regex pattern("((R?:DRAW|PLAY)\s([1-9]\d{0,1})\sCARD\(S\))"); //gradescope version
     //gradescope accepts std::regex pattern("((R?:DRAW|PLAY)\s([1-9]\d{0,1})\sCARD\(S\))"); but when u try this in vscode it doesnt work, prolly compiler issue
 
-    if (!std::regex_match(getInstruction(), pattern)) //if instruction does NOT match this pattern, its not playable so return false
+    std::regex pattern(R"((?:DRAW|PLAY)\s([1-9]\d{0,1})\sCARD\(S\))");
+
+    if (!std::regex_match(getInstruction(), pattern)) 
     {
         return false;
     }
